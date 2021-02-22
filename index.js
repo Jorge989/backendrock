8c9c413d9242e44c74cd5df1654ee83514c0dc60
Jorge989 <attiejorge89@gmail.com>
2021-02-22 19:51:14 -0300

    djkd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 // const __dirname = path.resolve();
 app.use("/", express.static(path.resolve(__dirname, "public")));
-
+console.log("Jorge attie")
 
 app.get("/", (request, response) => {
   const musics = [
