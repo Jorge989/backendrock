f9db2aec4d3f1ce57b15fc64ddd24a24299c8b9d
Jorge989 <attiejorge89@gmail.com>
2021-03-14 13:55:25 -0300

    arrumandobug

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import express from "express";
 const PORT = process.env.PORT || 3334; // <-  inclusive a porta que ele deixa exposta pro teu app
 // const port = portaDoAmbiente ou a 3334, se não tiver uma
 const HOST = `http://localhost:${PORT}`;
-
+console.log("oi");
 const app = express();
 app.use(cors());
 // const __dirname = path.resolve();
 app.use("/", express.static(path.resolve(__dirname, "public")));
-console.log("Jorge attie")
+console.log("Jorge attie");
 
 app.get("/", (request, response) => {
   const musics = [
@@ -19,63 +19,61 @@ app.get("/", (request, response) => {
       artist: "Deep Purple",
       name: "Smoke On The Water",
       duration: "00:06:09",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/Smoke.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/Smoke.mp3`,
     },
+
     {
       artist: "Queen of the Stone Age",
       name: "3's & 7's",
       duration: "00:06:09",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/Queens.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/Queens.mp3`,
     },
     {
       artist: "Metallica",
       name: "Nothing else matters",
       duration: "00:06:29",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/metallica_nothing_else_matters.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/metallica_nothing_else_matters.mp3`,
     },
     {
       artist: "KISS",
       name: "I Was Made For Lovin You",
       duration: "00:04:33",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/kiss.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/kiss.mp3`,
     },
     {
       artist: "Nirvana",
       name: "Smells like teen spirit",
       duration: "00:05:01",
-      cover: `https://rockerback.herokuapp.com/static/covers/nirvana_smells_like_teen_spirit.jpg`,
-      url: `https://rockerback.herokuapp.co/static/musics/nirvana_smells_like_teen_spirit.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/nirvana_smells_like_teen_spirit.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/nirvana_smells_like_teen_spirit.mp3`,
     },
-  
+
     {
       artist: "Black Sabbath",
       name: "Paranoid",
       duration: "00:04:28",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/black.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/black.mp3`,
     },
 
     {
       artist: "LedZepplin",
       name: "Black Dog",
       duration: "00:05:31",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/LedZepplin.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/LedZepplin.mp3`,
     },
     {
       artist: "Jimi Hendrix",
       name: "Izabella",
       duration: "00:04:28",
-      cover: `https://rockerback.herokuapp.com/static/covers/metallica_nothing_else_matters.jpg`,
-      url: `https://rockerback.herokuapp.com/static/musics/Jimi.mp3`,
+      cover: `https://rockerback.herokuapp.com/covers/metallica_nothing_else_matters.jpg`,
+      url: `https://rockerback.herokuapp.com/musics/Jimi.mp3`,
     },
-  
-   
-   
   ];
 
   return response.json({
@@ -88,8 +86,7 @@ app.use("*", (request, response) =>
   response.status(404).json({ message: "Oops, nothing to show here." })
 );
 
-console.log(process.env)
-
+console.log(process.env);
 
 // São suas variaveis de ambiente, o heroku tem lá as dele tambem
 
